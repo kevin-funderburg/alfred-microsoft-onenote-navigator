d12a7b03159a59488cacf29a9bc9f231810c1049
Kevin Funderburg <ksfunderburg@icloud.com>
2019-06-18 22:34:39 -0500

    strings working, still having issue loading settings

--- getNotebooks.py
+++ getNotebooks.py
@@ -7,33 +7,34 @@ import os
 import sys
 import re
 import argparse
+import plistlib
 import json
 import subprocess
 
 from workflow import Workflow3, ICON_INFO, ICON_WARNING
-from unicodedata import normalize
 
 __version__ = '1.1'
 
 wf = None
 log = None
-HELP_URL = ('https://github.com/kevin-funderburg/alfred-microsoft-onenote-navigator',
-            '#alfred-microsft-onenote-navigator')
+HELP_URL = 'https://github.com/kevin-funderburg/alfred-microsoft-onenote-navigator'
 
-UPDATE_SETTINGS={
-        'github_slug': 'kevin-funderburg/alfred-microsoft-onenote-navigator',
-}
-# DEFAULT_DATA_FILE=
-# DEFAULT_SETTINGS={
-#     'urlbase': None
-# }
+# GitHub repo for self-updating
+UPDATE_SETTINGS={'github_slug': 'kevin-funderburg/alfred-microsoft-onenote-navigator'}
+
+DEFAULT_SETTINGS={'urlbase': None}
+
 ONENOTEPLIST = "~/Library/Group Containers/UBF8T346G9.Office/OneNote/ShareExtension/Notebooks.plist"
 ICON_APP = "/Applications/Microsoft OneNote.app/Contents/Resources/OneNote.icns"
 
 
+
+
 def main(wf):
 
-    import plistlib
+    urlplist = wf.datadir + "/onenoteinfo.plist"
+    if not os.path.exists(urlplist):
+        plistlib.writePlist({"urlbase": ""}, urlplist)
 
     if wf.update_available:
         # Add a notification to top of Script Filter results
@@ -41,39 +42,30 @@ def main(wf):
                     'Action this item to install the update',
                     autocomplete='workflow:update',
                     icon=ICON_INFO)
-
     # wf.clear_settings()
     # build argument parser to parse script args and collect their
     # values
     parser = argparse.ArgumentParser()
     # add an optional (nargs='?') --seturl argument and save its
-    # value to 'urlbase' (dest). This will be called from a separate "Run Script"
-    # action with the API key
+    # value to 'urlbase' (dest).
     parser.add_argument('--seturl', dest='urlbase', nargs='?', default=None)
-    # parser.add_argument('--notebooks', dest='notebooks', nargs='?', default=None)
     parser.add_argument('--type', dest='type', nargs='?', default=None)
-    # parser.add_argument('--sections', dest='sections', nargs='?', default=None)
-    # add an optional query and save it to 'query'
     parser.add_argument('query', nargs='?', default=None)
-    # parse the script's arguments
     args = parser.parse_args(wf.args)
 
     ####################################################################
     # Save the provided URL base
     ####################################################################
 
-    # decide what to do based on arguments
     if args.urlbase:  # Script was passed as a URL
         if 'onenote:https' in args.urlbase:
-            args.urlbase = subprocess.check_output(re.search('(onenote.*/Documents/).*', args.urlbase).group(1))
-            # save the URL
-            # output = wf.decode(output)
-            # settings['urlbase'] = "onenote:https://d.docs.live.net/9478a1a4ec3795b7/Documents/"
-
-            # return 0
+            # extract onenote url
+            args.urlbase = re.search('(onenote.*/Documents/).*', args.urlbase).group(1)
+            wf.settings['urlbase'] = args.urlbase
+            return 0
         else:
             wf.add_item("The argument is not a OneNote URL string",
-                        "right click a OneNote page and click 'Copy Link to Page'",
+                        "right click a OneNote page/section/notebook and click 'Copy Link to Page' and try again",
                         valid=False,
                         icon=ICON_WARNING)
             wf.send_feedback()
@@ -83,28 +75,21 @@ def main(wf):
     # Check that we have a URL saved
     ####################################################################
     try:
-        # urlbase = wf.settings['urlbase']
         urlbase = wf.settings.get('urlbase', None)
     except "URL Not Found":
-            wf.add_item('No URL set yet.',
-                        'Please use urlsetkey to set your OneNote url base.',
-                        valid=False,
-                        icon=ICON_WARNING)
-            wf.send_feedback()
-            return 0
+        wf.add_item('No URL set yet.',
+                    'Please use seturl to set your OneNote url base.',
+                    valid=False,
+                    icon=ICON_WARNING)
+        wf.send_feedback()
+        return 0
 
     if args.type == 'notebook':
-        # urlbase = "onenote:https://d.docs.live.net/9478a1a4ec3795b7/Documents/"
+        # get plist data from OneNote plist
         one_note_pl = plistlib.readPlist(os.path.expanduser(ONENOTEPLIST))
-        # write all notebook plist data to data.plist for getNotebookSections.py
+        # write all notebook plist data to data.plist for later iterations
         plistlib.writePlist(one_note_pl, wf.datafile('data.plist'))
-        # urlbase = wf.encode(urlbase)
-        # log.debug(c)
-        # log.debug("urlbase is: " + wf.settings.get('ulbase'), None)
 
-        # with open(wf.settings_path, "r") as read_file:
-        #     settings = json.load(read_file)
-        # urlbase = settings.get()
         for n in one_note_pl:
             it = wf.add_item(title=n["Name"],
                              subtitle="View " + n["Name"] + "'s sections",
@@ -114,11 +99,13 @@ def main(wf):
                              icon="icon.png",
                              icontype="file",
                              quicklookurl=ICON_APP)
-            # urlbase = str(urlbase + n["Name"])
             it.setvar('subPreFix', "")
             it.setvar('notebook', n["Name"])
             it.setvar('theURL', "")
-            it.add_modifier('cmd', subtitle = wf.settings['urlbase'], arg = wf.settings['urlbase'], valid=True)
+            it.add_modifier('cmd',
+                            subtitle="{0}{1}".format(wf.settings.get('urlbase'), n["Name"]),
+                            arg="{0}{1}".format(wf.settings.get('urlbase'), n["Name"]),
+                            valid=True)
 
         # if len(wf.items) == 0:
         #     wf.add_item('No notebooks found', icon=ICON_WARNING)
@@ -131,10 +118,15 @@ def main(wf):
     if args.type == 'section':
         notebook = os.getenv('notebook')
         q = os.getenv('q')
+        # notebook = None
+        # q = 'Mac'
         if notebook is None:
             notebook = q
-
         found = False
+        # one_note_pl = plistlib.readPlist(os.path.expanduser(ONENOTEPLIST))
+        # # write all notebook plist data to data.plist for later iterations
+        # plistlib.writePlist(one_note_pl, wf.datafile('data.plist'))
+        pl = plistlib.readPlist(wf.datafile('data.plist'))
         for p in pl:
             if p["Name"] == q:
                 found = True
@@ -152,14 +144,26 @@ def main(wf):
                 theURL = os.getenv("theURL")
 
                 if (subPreFix == "") or (subPreFix is None):
-                    subPreFix = notebook + " > " + c["Name"]
+                    # subPreFix = notebook + " > " + c["Name"]
+                    subPreFix = "{0} > {1}".format(notebook, c["Name"])
                 else:
-                    subPreFix = subPreFix + " > " + c["Name"]
+                    subPreFix = "{0} > {1}".format(subPreFix, c["Name"])
+                    # subPreFix = subPreFix + " > " + c["Name"]
 
-                if (theURL == "") or (theURL is None):
-                    theURL = urlbase + notebook + "/" + c["Name"]
+                if wf.settings is None:
+                    log.debug("settings is None")
                 else:
-                    theURL = theURL + "/" + c["Name"]
+                    log.debug("settings is not None")
+
+                settings = wf.settings
+                test = wf.decode(subprocess.check_output(settings.get('urlbase')))
+                log.debug(test)
+                if (theURL == "") or (theURL is None):
+                    theURL = "{0}{1}/{2}".format(test, notebook, c["Name"])
+                    # theURL = urlbase + notebook + "/" + c["Name"]
+                else:
+                    theURL = "{0}/{1}".format(theURL, c["Name"])
+                    # theURL = theURL + "/" + c["Name"]
 
                 it = wf.add_item(title=c["Name"],
                                  subtitle=subPreFix,
@@ -188,10 +192,10 @@ def main(wf):
                              quicklookurl=ICON_APP)
             it.setvar('leaf', "true")
 
-        if len(wf.items) == 0:
-            wf.add_item('No sections found', icon=ICON_WARNING)
-            wf.send_feedback()
-            return 0
+        # if len(wf.items) == 0:
+        #     wf.add_item('No sections found', icon=ICON_WARNING)
+        #     wf.send_feedback()
+        #     return 0
 
         wf.send_feedback()
         return 0
